c429da3cff215518e1fb6e0ec9decf9191b4dbd1
erika <erika.lizbeth994@gmail.com>
2015-05-22 00:56:56 -0500

    Agregar Usuarios
    Se agrego lo referente a agregar usuarios

--- UmaiSystem2/src/Controllers/Usuarios/AgregarUsuariosController.java
+++ UmaiSystem2/src/Controllers/Usuarios/AgregarUsuariosController.java
@@ -5,19 +5,27 @@
  */
 package Controllers.Usuarios;
 
+import com.mysql.jdbc.ResultSet;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
  *
- * @author lalo
+ * @author erika
  */
 public class AgregarUsuariosController implements Initializable {
     @FXML
@@ -48,65 +56,204 @@ public class AgregarUsuariosController implements Initializable {
     private Button btnCancelar;
     @FXML
     private Button btnAceptar;
-
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+        Models.CRUD cr= new Models.CRUD();
+          ResultSet resul=null;  
+            resul=(ResultSet) cr.Consulta("Select idUsu,nombreUsu from usuarios");
+        
+        txtNomUsu.textProperty().addListener(new ChangeListener<String>(){
+        
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+            if(newValue.matches("/^[a-z0-9_-]{3,15}$/")){
+                txtNomUsu.setText(newValue);
+            }
+            else
+            {
+                txtNomUsu.setText(oldValue);
+            }
+            }
+        });
+        txtContraseñaUsu.textProperty().addListener(new ChangeListener<String>() {
 
-    @FXML
-    private void txtNomUsuarioClick(ActionEvent event) {
-    }
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+           if(newValue.matches("/(^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d){4,10}.+$)/")){
+                txtContraseñaUsu.setText(newValue);
+            }
+            else
+            {
+                txtContraseñaUsu.setText(oldValue);
+            }
+            }
+        });
+        
+        txtCalleUsu.textProperty().addListener(new ChangeListener<String>(){
 
-    @FXML
-    private void txtContraseñaUsuClick(ActionEvent event) {
-    }
-
-    @FXML
-    private void txtCalleUsuClick(ActionEvent event) {
-    }
-
-    @FXML
-    private void txtColoniaUsuClick(ActionEvent event) {
-    }
-
-    @FXML
-    private void txtCiudadUsuClick(ActionEvent event) {
-    }
-
-    @FXML
-    private void txtCpUsuClick(ActionEvent event) {
-    }
-
-    @FXML
-    private void txtTelefonoUsuClick(ActionEvent event) {
-    }
-
-    @FXML
-    private void txtSueldoUsuClick(ActionEvent event) {
-    }
-
-    @FXML
-    private void txtPuntosUsuClick(ActionEvent event) {
-    }
-
-    @FXML
-    private void txtPermisoUsu(ActionEvent event) {
-    }
-
-    @FXML
-    private void txtContratoUsuClick(ActionEvent event) {
-    }
-
+             @Override
+             public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if(newValue.matches("[A-Aa-z]*\\s*[A-Za-z]*")){
+                    txtCalleUsu.setText(newValue);
+                }else{
+                    txtCalleUsu.setText(oldValue);
+                }
+             }
+            
+        });
+        txtColoniaUsu.textProperty().addListener(new ChangeListener<String>(){
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if(newValue.matches("[A-Aa-z]*\\s*[A-Za-z]*")){
+                    txtColoniaUsu.setText(newValue);
+                }else{
+                    txtColoniaUsu.setText(oldValue);
+                }
+             }
+        });
+        txtCiudadUsu.textProperty().addListener(new ChangeListener<String>(){
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if(newValue.matches("[A-Aa-z]*\\s*[A-Za-z]*")){
+                    txtCiudadUsu.setText(newValue);
+                }else{
+                    txtCiudadUsu.setText(oldValue);
+                }
+             }
+        });
+        txtCpUsu.textProperty().addListener(new ChangeListener<String>(){
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if(newValue.matches("^([1-9]{2}|[0-9][1-9]|[1-9][0-9])[0-9]{3}$")){
+                    txtCpUsu.setText(newValue);
+                }else{
+                    txtCpUsu.setText(oldValue);
+                }
+             }
+        });
+        txtTelefonoUsu.textProperty().addListener(new ChangeListener<String>(){
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if(newValue.matches("^[0-9]{2,3}-? ?[0-9]{10}$")){
+                    txtTelefonoUsu.setText(newValue);
+                }else{
+                    txtTelefonoUsu.setText(oldValue);
+                }
+             }
+        });
+        txtSueldoUsu.textProperty().addListener(new ChangeListener<String>(){
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if(newValue.matches("[0-9]*")){
+                    txtSueldoUsu.setText(newValue);
+                }else{
+                    txtSueldoUsu.setText(oldValue);
+                }
+             }
+        });
+        txtPuntosUsu.textProperty().addListener(new ChangeListener<String>(){
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if(newValue.matches("[0-9]*")){
+                    txtPuntosUsu.setText(newValue);
+                }else{
+                    txtPuntosUsu.setText(oldValue);
+                }
+             }
+        });
+        txtPermisoUsu.textProperty().addListener(new ChangeListener<String>(){
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if(newValue.matches("[A-Aa-z]*\\s*[A-Za-z]*")){
+                    txtPermisoUsu.setText(newValue);
+                }else{
+                    txtPermisoUsu.setText(oldValue);
+                }
+             }
+        });
+        txtContratoUsu.textProperty().addListener(new ChangeListener<String>(){
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if(newValue.matches("[A-Aa-z]*\\s*[A-Za-z]*")){
+                    txtContratoUsu.setText(newValue);
+                }else{
+                    txtContratoUsu.setText(oldValue);
+                }
+             }
+        });
+    }   
     @FXML
     private void btnCancelarClick(ActionEvent event) {
     }
 
     @FXML
-    private void btnAceptarClick(ActionEvent event) {
+    private void btnAceptarClick(ActionEvent event) 
+    {
+        Models.ModelUsuarios usuarios=new Models.ModelUsuarios();
+        String mensageE="Campos: ";
+          if(txtNomUsu.getText().equals("")){
+              mensageE+="Nombre";
+          }
+          if(txtContraseñaUsu.getText().equals("")){
+              mensageE+=", Contraseña";
+          }
+          if(txtCalleUsu.getText().equals("")){
+              mensageE+=", Calle";
+          }
+          if(txtColoniaUsu.getText().equals("")){
+              mensageE+=", Colonia";
+          }
+         if(txtCiudadUsu.getText().equals("")){
+              mensageE+=", Ciudad";
+          }
+         if(txtCpUsu.getText().equals("")){
+              mensageE+=", C.P.";
+          }
+         if(txtTelefonoUsu.getText().equals("")){
+              mensageE+=", Telefono";
+          }
+         if(txtSueldoUsu.getText().equals("")){
+              mensageE+=", Sueldo";
+          }
+         if(txtPuntosUsu.getText().equals("")){
+              mensageE+=", Puntuación";
+          }
+         if(txtPermisoUsu.getText().equals("")){
+              mensageE+=", Permiso";
+          }
+         if(txtContratoUsu.getText().equals("")){
+              mensageE+=", Contrato";
+          }
+          mensageE+=" vacios";
+          lblError.setText(mensageE);
+           if(   !txtNomUsu.getText().equals("")
+           && !txtContraseñaUsu.getText().equals("")
+           && !txtCalleUsu.getText().equals("")
+           && !txtColoniaUsu.getText().equals("")
+           && !txtCiudadUsu.getText().equals("")
+           && !txtCpUsu.getText().equals("")
+           && !txtTelefonoUsu.getText().equals("")
+           && !txtSueldoUsu.getText().equals("")
+           && !txtPuntosUsu.getText().equals("") 
+           && !txtPermisoUsu.getText().equals("")
+           && !txtContratoUsu.getText().equals("")
+          )
+        {
+        usuarios.setNombreUsu(txtNomUsu.getText());
+        usuarios.setContraseñaUsu(txtContraseñaUsu.getText());
+        usuarios.setCalleUsu(txtCalleUsu.getText());
+        usuarios.setColoUsu(txtColoniaUsu.getText());
+        usuarios.setCiudUsu(txtCiudadUsu.getText());
+        usuarios.setCodigoPostal(txtCpUsu.getText());
+        usuarios.setTelefonoUsu(Integer.parseInt(txtTelefonoUsu.getText()));
+        usuarios.setSueldoUsu(Integer.parseInt(txtSueldoUsu.getText()));
+        usuarios.setPuntosUsu(Integer.parseInt(txtPuntosUsu.getText()));
+        usuarios.setTipoPermiso(txtPermisoUsu.getText());
+        int idProv=1;
+         Models.CRUD cr=new Models.CRUD();
+            ResultSet rs=null;
+            
+            
+            
+       
+       usuarios.agregarUsuarios();
+    }
     }
     
 }
--- UmaiSystem2/src/Models/ModelUsuarios.java
+++ UmaiSystem2/src/Models/ModelUsuarios.java
@@ -4,15 +4,257 @@
  * and open the template in the editor.
  */
 package Models;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 /**
  *
  * @author lalo
  */
 public class ModelUsuarios {
-
-    public ModelUsuarios() {
+    
+    private  SimpleIntegerProperty idUsu = new SimpleIntegerProperty();
+    private SimpleStringProperty nombreUsu = new SimpleStringProperty();
+    private  SimpleStringProperty contraseñaUsu = new SimpleStringProperty();
+    private  SimpleStringProperty calleUsu = new SimpleStringProperty();
+    private  SimpleStringProperty coloUsu = new SimpleStringProperty();
+    private  SimpleStringProperty ciudUsu = new SimpleStringProperty();
+    private  SimpleIntegerProperty codigoPostal = new SimpleIntegerProperty();
+    private  SimpleIntegerProperty telefonoUsu = new SimpleIntegerProperty();
+    private  SimpleIntegerProperty sueldoUsu = new SimpleIntegerProperty();
+    private  SimpleIntegerProperty puntosUsu = new SimpleIntegerProperty();
+    private  SimpleStringProperty tipoPermiso = new SimpleStringProperty();
+    private  SimpleStringProperty estadoContrato = new SimpleStringProperty();
+    
+    CRUD cr=new CRUD();
+    
+    public ModelUsuarios() 
+    {
     }
- 
+    
+    public ModelUsuarios(SimpleIntegerProperty idUsu, SimpleStringProperty nombreUsu, 
+            SimpleStringProperty contraseñaUsu, SimpleStringProperty calleUsu, 
+            SimpleStringProperty coloUsu, SimpleStringProperty ciudUsu,
+            SimpleIntegerProperty codigoPostal, SimpleIntegerProperty telefonoUsu,
+            SimpleIntegerProperty sueldoUsu, SimpleIntegerProperty puntosUsu,
+            SimpleStringProperty tipoPermiso, SimpleStringProperty estadoContrato
+            ) {
+        this.idUsu = idUsu;
+        this.nombreUsu = nombreUsu;
+        this.contraseñaUsu = contraseñaUsu;
+        this.calleUsu = calleUsu;
+        this.coloUsu = coloUsu;
+        this.ciudUsu= ciudUsu;
+        this.codigoPostal = codigoPostal;
+        this.telefonoUsu = telefonoUsu;
+        this.sueldoUsu = sueldoUsu;
+        this.puntosUsu = puntosUsu;
+        this.tipoPermiso = tipoPermiso;
+        this.estadoContrato = estadoContrato;
+    }
+    
+    public void agregarUsuarios()
+    {
+       
+    cr.insertar("call ProcInsertarUsuario("
+                    + "'"+getNombreUsu()+"',"
+                    + "'" +getContraseñaUsu()+"',"
+                    +getCalleUsu()+","
+                    +getColoUsu()+","
+                    +"'"+getCiudUsu()+"',"
+                    +getCodigoPostal()+"',"
+                    +getTelefonoUsu()+"',"
+                    +getSueldoUsu()+"',"
+                    +getPuntosUsu()+"',"
+                    +getTipoPermiso()+"',"
+                    +getEstadoContrato()+")");
+      
+    }
+    /**
+     * @return the idUsu
+     */
+    public int getIdUsu() {
+        return idUsu.get();
+    }
+    
+    /**
+     * @param idUsu the idUsu to set
+     */
+    public void setIdUsu(int idUsu) {
+        this.idUsu.set(idUsu);
+    }
+    
+    /**
+     * @return the nombreUsu
+     */
+    public String getNombreUsu() {
+        return nombreUsu.get();
+    }
+
+    /**
+     * @param nombreUsu the nombreUsu to set
+     */
+    public void setNombreUsu(String nombreUsu) {
+      
+       this.nombreUsu.set(nombreUsu);
+    }
+    
+    /**
+     * @return the contraseñaUsu
+     */
+    public String getContraseñaUsu() {
+        return contraseñaUsu.get();
+    }
+
+    /**
+     * @param contraseñaUsu the contraseñaUsu to set
+     */
+    public void setContraseñaUsu(String contraseñaUsu) {
+      
+       this.contraseñaUsu.set(contraseñaUsu);
+    }
+    
+    /**
+     * @return the calleUsu
+     */
+    public String getCalleUsu() {
+        return calleUsu.get();
+    }
+
+    /**
+     * @param calleUsu the calleUsu to set
+     */
+    public void setCalleUsu(String calleUsu) {
+      
+       this.calleUsu.set(calleUsu);
+    }
+    
+    /**
+     * @return the coloUsu
+     */
+    public String getColoUsu() {
+        return coloUsu.get();
+    }
+
+    /**
+     * @param coloUsu the coloUsu to set
+     */
+    public void setColoUsu(String coloUsu) {
+      
+       this.coloUsu.set(coloUsu);
+    }
+    
+    /**
+     * @return the ciudUsu
+     */
+    public String getCiudUsu() {
+        return ciudUsu.get();
+    }
+
+    /**
+     * @param ciudUsu the ciudUsu to set
+     */
+    public void setCiudUsu(String ciudUsu) {
+      
+       this.ciudUsu.set(ciudUsu);
+    }
+    
+    /**
+     * @return the codigoPostal
+     */
+    public int getCodigoPostal() {
+        return codigoPostal.get();
+    }
+
+    /**
+     * @param codigoPostal the codigoPostal to set
+     */
+    public void setCodigoPostal(String codigoPostal) {
+      
+       this.ciudUsu.set(codigoPostal);
+    }
+    
+    /**
+     * @return the telefonoUSu
+     */
+    public int getTelefonoUsu() {
+        return telefonoUsu.get();
+    }
+
+    /**
+     * @param telefonoUsu the telefonoUsu to set
+     */
+    public void setTelefonoUsu(int telefonoUsu) {
+      
+       this.telefonoUsu.set(telefonoUsu);
+    }
+    
+    /**
+     * @return the sueldoUsu
+     */
+    public int getSueldoUsu() {
+        return sueldoUsu.get();
+    }
+
+    /**
+     * @param sueldoUsu the sueldoUsu to set
+     */
+    public void setSueldoUsu(int sueldoUsu) {
+      
+       this.sueldoUsu.set(sueldoUsu);
+    }
+    
+    /**
+     * @return the puntosUsu
+     */
+    public int getPuntosUsu() {
+        return puntosUsu.get();
+    }
+
+    /**
+     * @param puntosUsu the puntosUsu to set
+     */
+    public void setPuntosUsu(int puntosUsu) {
+      
+       this.puntosUsu.set(puntosUsu);
+    }
+    
+    /**
+     * @return the tipoPermiso
+     */
+    public String getTipoPermiso() {
+        return tipoPermiso.get();
+    }
+
+    /**
+     * @param puntosUsu the puntosUsu to set
+     */
+    public void setTipoPermiso(String tipoPermiso) {
+      
+       this.tipoPermiso.set(tipoPermiso);
+    }
+    
+    /**
+     * @return the estadoContrato
+     */
+    public String getEstadoContrato() {
+        return estadoContrato.get();
+    }
+
+    /**
+     * @param estadoContrato the estadoContrato to set
+     */
+    public void setEstadoContrato(String estadoContrato) {
+      
+       this.estadoContrato.set(estadoContrato);
+    }
+
+   
     
 }
